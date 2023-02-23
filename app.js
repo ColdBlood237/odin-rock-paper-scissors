7ecdf5086cea7f92551efb7584b8773848044ca1
ColdBlood237 <115800548+ColdBlood237@users.noreply.github.com>
2023-02-23 17:42:55 +0100

    Get player choice and evaluate all possible outcomes

--- app.js
+++ app.js
@@ -5,7 +5,51 @@ function getComputerChoice() {
     return options[choice];
 }
 
+// function that ask the user to play
+function getPlayerChoice() {
+    let choice = prompt("Rock, Paper or Scissors");
+    return choice;
+}
+
 // plays a single round and determine the winner
-// function round(playerSelection, computerSelection) {
-    
-// }
+function round(playerSelection, computerSelection) {
+    let playerChoice = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
+    let computerChoice = computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1).toLowerCase();
+    let message = "";
+
+    // evaluate all the possible outcomes 
+    if (playerChoice === "Rock") {
+        if (computerChoice === "Rock") {
+            message = "It's a Tie!";
+        } else if (computerChoice === "Paper") {
+            message = "You Lose! Paper beats Rock";
+        } else if (computerChoice === "Scissors") {
+            message = "You Win! Rock beats Scissors";
+        }
+    }
+    else if (playerChoice === "Paper") {
+        if (computerChoice === "Rock") {
+            message = "You Win! Paper beats Rock";
+        } else if (computerChoice === "Paper") {
+            message = "It's a Tie!";
+        } else if (computerChoice === "Scissors") {
+            message = "You Lose! Scissors beat Paper";
+        }
+    }
+    else if (playerChoice === "Scissors") {
+        if (computerChoice === "Rock") {
+            message = "You Lose! Rock beats Scissors";
+        } else if (computerChoice === "Paper") {
+            message = "You Win! Scissors beat Paper";
+        } else if (computerChoice === "Scissors") {
+            message = "It's a Tie!";
+        }
+    }
+
+    return message;
+}
+
+const playerSelection = getPlayerChoice();
+const computerSelection = getComputerChoice();
+
+console.log(round(playerSelection, computerSelection));
